de284d06c6d6898a1193eb0e2e0e3b0cc8b6a329
Antoine <me@antoinebellanger.fr>
2024-01-29 15:00:28 +0100

    [ENHANCEMENT] Add example

--- examples/check_stdout.c
+++ examples/check_stdout.c
@@ -13,3 +13,11 @@ Test(example, default, .init = cr_redirect_stdout) {
     printf("Hello World!\n");
     cr_assert_stdout_eq_str("Hello World!\n");
 }
+
+Test(example, own_main, .init = cr_redirect_stdout) {
+    char argv0[] = "is not needed";
+    char argv1[] = "Rail Safety";
+    char argv2[] = "Fairy tales";
+    char *argvs[] = {&argv0, &argv1, &argv2};
+    cr_assert_eq(0, anagram(3, argvs));
+}
